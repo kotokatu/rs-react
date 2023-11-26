be0f09168a12257cbda4f031b4c9b9ac466b74e9
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-11-26 22:56:34 +0300

    refactor: remove files

--- src/__tests__/test-utils/getPlayersFetchMock.ts
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse } from 'msw';
-import { baseUrl } from '@/constants/constants';
-import { mockApiData } from '../mocks/mockData';
-
-export const getPlayerstHandler = () =>
-  http.get(baseUrl, () => {
-    HttpResponse.json(mockApiData);
-  });
