454e175321211511312a7216612e93dd4960bad9
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-12-03 20:07:56 +0300

    refactor: change special chars regex

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,4 +3,4 @@ export const VALID_FILE_SIZE = 102400;
 export const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const NAME_REGEX = /^\p{Lu}\p{L}*/gu;
-export const SPECIAL_CHARS_REGEX = /^(?=.*[~`! @#$%^&*()-_+={}[\]|;:"<>,./?])/;
+export const SPECIAL_CHARS_REGEX = /^(?=.*[~`!@#$%^&*()-_+={}[\]|;:"<>,./?])/;
